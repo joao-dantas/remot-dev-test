9573afc0a6f7b7b326d20dad95f1145607dfdbd8
Joao Dantas <joao.dantas@zwift.com>
2020-07-28 07:20:39 -0300

    Use application default image.

--- run-dev-env.sh
+++ run-dev-env.sh
@@ -9,13 +9,15 @@ function cleanup() {
 
 cleanup
 
-docker build -f Dockerfile.dev -t remote-dev-test-builder .
+docker build -f ./src/main/docker/Dockerfile.jvm -t remote-dev-test-builder .
 
 docker run -d --rm \
   --name remote-dev-test-svc \
   -p 8080:8080 \
   -v ~/.m2/:/root/.m2/ \
-  remote-dev-test-builder
+  --env QUARKUS_LAUNCH_DEVMODE=true \
+  remote-dev-test-builder \
+  -Dquarkus.live-reload.password=123
 
 # wait for app startup
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/health)" != "200" ]]; do

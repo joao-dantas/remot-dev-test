1679e2e921bb6662dc2c7ea6261caabd162a0144
Joao Dantas <joao.dantas@zwift.com>
2020-07-29 11:09:19 -0300

    Working.

--- run-dev-env.sh
+++ run-dev-env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-
 trap cleanup EXIT
 
 function cleanup() {
@@ -9,17 +8,20 @@ function cleanup() {
 
 cleanup
 
-docker build -f Dockerfile.dev -t remote-dev-test-builder .
+docker build -f src/main/docker/Dockerfile.fast-jar -t remote-dev-test-builder .
 
 docker run -d --rm \
   --name remote-dev-test-svc \
   -p 8080:8080 \
-  -v ~/.m2/:/root/.m2/ \
-  remote-dev-test-builder
+  -e QUARKUS_LAUNCH_DEVMODE=true \
+  remote-dev-test-builder -Dquarkus.live-reload.password=123
 
 # wait for app startup
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/health)" != "200" ]]; do
+  echo "not ready yet"
   sleep 2;
 done
+
+echo "ready!"
 
 ./mvnw compile quarkus:remote-dev -Dquarkus.live-reload.url=http://localhost:8080 -Dquarkus.live-reload.password=123
